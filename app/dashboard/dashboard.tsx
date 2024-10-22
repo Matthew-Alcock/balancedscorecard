ccc5ac9ff29c71789b0fa4eb3c62f5aed205dd7b
Matthew-Alcock <matthewalcock@gmail.com>
2024-10-22 09:20:51 +0200

    update paths

--- app/dashboard/dashboard.tsx
+++ app/dashboard/dashboard.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import React, { useEffect, useState } from 'react';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/app/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/app/components/ui/tabs";
+import { Button } from "@/app/components/ui/button";
 import { PlusCircle, Users, Building, User } from 'lucide-react';
 import Link from 'next/link';
 import { supabase } from '@/lib/supabaseClient';
